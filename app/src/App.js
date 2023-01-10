2c8c89d7aa28516604cebc958e18b829e1d22efb
Talgat Tokayev <59330081+Talgat1164@users.noreply.github.com>
2023-01-11 01:18:12 +0600

    useInput

---
+++ app/src/App.js
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+
+const App = () => {
+  const username = useInput("");
+  const password = useInput("");
+
+  return (
+    <div>
+      <input type="text" {...username} placeholder="Username" />
+      <input type="text" {...password} placeholder="Password" />
+      <button onClick={() => console.log(username.value, password.value)}>
+        Click
+      </button>
+    </div>
+  );
+};
+
+export default App;
