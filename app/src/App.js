1e80213ecdbabfad9fa342461aa62af6f7c2babb
Talgat Tokayev <59330081+Talgat1164@users.noreply.github.com>
2023-01-11 15:46:49 +0600

    useHover

--- app/src/App.js
+++ app/src/App.js
@@ -1,17 +1,23 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
+import Hover from "./components/Hover";
+import useHover from "./hooks/useHover";
 import useInput from "./hooks/useInput";
 
 const App = () => {
-  const username = useInput("");
-  const password = useInput("");
-
+  const ref = useRef();
+  const isBlackHovering = useHover(ref);
   return (
     <div>
-      <input type="text" {...username} placeholder="Username" />
-      <input type="text" {...password} placeholder="Password" />
-      <button onClick={() => console.log(username.value, password.value)}>
-        Click
-      </button>
+      <Hover />
+      <div
+        ref={ref}
+        style={{
+          width: 300,
+          height: 300,
+          background: isBlackHovering ? "blue" : "green",
+          marginTop: 10,
+        }}
+      ></div>
     </div>
   );
 };
