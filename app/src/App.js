fc96c59b348a53861588e8d5e96e0f14ad2f2e87
Talgat Tokayev <59330081+Talgat1164@users.noreply.github.com>
2023-01-11 16:13:04 +0600

    useScroll/useInfinitePagination

--- app/src/App.js
+++ app/src/App.js
@@ -1,23 +1,10 @@
 import React, { useRef, useState } from "react";
-import Hover from "./components/Hover";
-import useHover from "./hooks/useHover";
-import useInput from "./hooks/useInput";
+import List from "./components/List";
 
 const App = () => {
-  const ref = useRef();
-  const isBlackHovering = useHover(ref);
   return (
     <div>
-      <Hover />
-      <div
-        ref={ref}
-        style={{
-          width: 300,
-          height: 300,
-          background: isBlackHovering ? "blue" : "green",
-          marginTop: 10,
-        }}
-      ></div>
+      <List />
     </div>
   );
 };
